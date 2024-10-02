c54cea4a39810819e8c3ff5d02700f7e9732768d
Sudhan <86077071+Sudhan-Dahake@users.noreply.github.com>
2024-10-02 00:24:38 -0400

    Implemented a Multi-Head Self Attention and FeedForward layer

--- mini-gpt.py
+++ mini-gpt.py
@@ -87,9 +87,35 @@ class Head(nn.Module):
             self.tril[:T, :T] == 0, float('-inf'))    # (B, T, T)
         wei = F.softmax(wei, dim=-1)    # (B, T, T)
         # perform the weighted aggregation of the values
-        v = self.value(x)    # (B, T, C)
-        out = wei @ v    # (B, T, T) @ (B, T, C) => (B, T, C)
+        v = self.value(x)    # (B, T, head_size)
+        out = wei @ v    # (B, T, T) @ (B, T, head_size) => (B, T, head_size)
+
         return out
+
+
+class MultiHeadAttention(nn.Module):
+    """ Multiple heads of self-attention in parallel """
+
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(head_size) for _ in range(num_heads)])
+
+    def forward(self, x):
+        return torch.cat([h(x) for h in self.heads], dim=-1)
+
+
+class FeedForward(nn.Module):
+    """ a simple linear layer followed by a non-linearity """
+
+    def __init__(self, n_embd):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_embd, n_embd),
+            nn.ReLU(),
+        )
+
+    def forward(self, x):
+        return self.net(x)
 
 
 # Super simple bigram model
@@ -100,17 +126,21 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.sa_heads = MultiHeadAttention(4, (n_embd // 4))                  # i.e. 4 heads of 8-dimensional self-attention
+        self.ffwd = FeedForward(n_embd)
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
         B, T = idx.shape
 
         # idx and targets are both (B, T) tensor of integers
-        tok_emb = self.token_embedding_table(idx)    # (B, T, C)
+        tok_emb = self.token_embedding_table(idx)     # (B, T, C)
         pos_emb = self.position_embedding_table(
-            torch.arange(T, device=device))    # (T, C)
-        x = tok_emb + pos_emb    # (B, T, C)
-        logits = self.lm_head(x)    # (B, T, vocab_size)
+            torch.arange(T, device=device))           # (T, C)
+        x = tok_emb + pos_emb                         # (B, T, C)
+        x = self.sa_heads(x)                          # apply one head of self-attention. (B, T, C)
+        x = self.ffwd(x)                              # (B, T, C) 
+        logits = self.lm_head(x)                      # (B, T, vocab_size)
 
         if targets is None:
             loss = None
@@ -125,8 +155,10 @@ class BigramLanguageModel(nn.Module):
     def generate(self, idx, max_new_tokens):
         # idx is (B, T) array of indices in the current context
         for _ in range(max_new_tokens):
+            # crop idx to the last block_size tokens
+            idx_cond = idx[:, -block_size:]
             # get the predictions
-            logits, loss = self(idx)
+            logits, loss = self(idx_cond)
             # focus only on the last time step
             logits = logits[:, -1, :]    # becomes (B, C)
             # apply softmax to get probabilities
@@ -151,8 +183,8 @@ for iter in range(max_iters):
     # every once in a while evaluate the loss on train and val sets
     if iter % eval_interval == 0:
         losses = estimate_loss()
-        print(f"step {iter}: train loss {
-              losses['train']: .4f}, val loss {losses['val']: .4f}")
+        print(
+            f"step {iter}: train loss {losses['train']: .4f}, val loss {losses['val']: .4f}")
 
     # sample a batch of data
     xb, yb = get_batch('train')
